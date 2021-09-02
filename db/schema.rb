87db4aaa7874071e295d9e9bdfdb2d30f0b0a64e
rafael boas <rafaelrvb@gmail.com>
2021-09-02 14:09:13 -0300

    trying another moedl

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_30_210104) do
+ActiveRecord::Schema.define(version: 2021_09_02_162353) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,6 +53,16 @@ ActiveRecord::Schema.define(version: 2021_08_30_210104) do
     t.index ["user_id"], name: "index_decks_on_user_id"
   end
 
+  create_table "studies", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.integer "card_id"
+    t.integer "repetition", default: 1
+    t.float "grade"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_studies_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -73,4 +83,5 @@ ActiveRecord::Schema.define(version: 2021_08_30_210104) do
   add_foreign_key "deck_reviews", "decks"
   add_foreign_key "deck_reviews", "users"
   add_foreign_key "decks", "users"
+  add_foreign_key "studies", "users"
 end
