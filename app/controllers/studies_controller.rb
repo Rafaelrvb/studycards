e6455db3e1276679624ac9f52da7504913461424
rafael boas <rafaelrvb@gmail.com>
2021-09-02 18:16:51 -0300

    almost done with study logic

--- app/controllers/studies_controller.rb
+++ app/controllers/studies_controller.rb
@@ -1,14 +1,29 @@
 class StudiesController < ApplicationController
 
   def new
-    @study = Study.new
     @deck_community = DeckCommunity.find(params[:deck_community_id])
     @cards = @deck_community.deck.cards
   end
 
   def create
-    raise
+    card = params[:card_id].to_i
+    difficulty = params[:grade].to_i/100.0
+    deck_community_id = params[:deck_community_id]
+
+
+
+
+    @study = Study.new(user_id: current_user.id, card_id: card, grade: difficulty)
+    @study.save
+
+    redirect_to new_study_path(deck_community_id)
+
+
+
+
   end
+
+
 
 
 end
